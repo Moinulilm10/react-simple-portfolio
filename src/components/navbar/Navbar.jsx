326d45a46f83eecc9c7fb507c65a7a7f48c94b6b
Moinul Islam <moinulilm10@gmail.com>
2024-03-08 04:00:18 +0600

    navbar responsive issue fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,74 +1,88 @@
-import {
-  Bars3Icon,
-  MoonIcon,
-  PaperAirplaneIcon,
-  SunIcon,
-} from "@heroicons/react/24/outline";
 import { useState } from "react";
+import "../../styles/Navbar.css";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <nav>
-      <div className="sticky mx-auto max-w-7xl">
-        <div className="flex justify-between w-5/6 mx-auto ">
-          {/* Primary menu and logo */}
-          <div className="flex items-center gap-16 my-12">
-            {/* logo */}
-            <div>
-              <a
-                href="/"
-                className="flex items-center gap-1 font-bold text-gray-700 "
-              >
-                <PaperAirplaneIcon className="w-6 h-6 text-primary" />
-                <span>Mi.</span>
-              </a>
-            </div>
-            {/* primary */}
-            <div className="hidden gap-8 lg:flex ">
-              <a href="#" className="">
-                Home
-              </a>
-              <a href="#">About</a>
-              <a href="#">Experience</a>
-              <a href="#">Project</a>
-              <a href="#">Contact </a>
-            </div>
-          </div>
-          {/* secondary */}
-          <div className="flex gap-6">
-            <div className="items-center hidden gap-10 xs:flex">
-              <div className="items-center hidden gap-2 lg:flex">
-                <MoonIcon className="w-6 h-6" />
-                <SunIcon className="w-6 h-6" />
-              </div>
-              <div>
-                <button className="">Free Trial</button>
-              </div>
-            </div>
-            {/* Mobile navigation toggle */}
-            <div className="flex items-center lg:hidden">
-              <button onClick={() => setToggleMenu(!toggleMenu)}>
-                <Bars3Icon className="h-6" />
-              </button>
-            </div>
-          </div>
+    <nav className="fixed top-0 z-20 w-full border-gray-200 dark:bg-gray-900 start-0 dark:border-gray-600">
+      <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Portfolio
+          </span>
+        </a>
+        <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 border rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Resume
+          </button>
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
         </div>
-      </div>
-      {/* mobile navigation */}
-      <div
-        className={`fixed z-40 w-full  bg-gray-100 overflow-hidden flex flex-col lg:hidden gap-12  origin-top duration-700 ${
-          !toggleMenu ? "h-0" : "h-full"
-        }`}
-      >
-        <div className="px-8">
-          <div className="flex flex-col gap-8 font-bold tracking-wider">
-            <a href="#">About</a>
-            <a href="#">Experience</a>
-            <a href="#">Project</a>
-            <a href="#">Contact </a>
-          </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block px-3 py-2 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                aria-current="page"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Experience
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Project
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
